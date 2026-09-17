98b8c2c38e3bf7d8d45aa1fcc5d7990cb962f3d3
rachit4018 <pandyarachit6@gmail.com>
2026-09-17 19:07:00 +0530

    fix: changed db config

--- backend/app/db.py
+++ backend/app/db.py
@@ -21,8 +21,7 @@ engine = create_async_engine(
     echo=False,
     pool_pre_ping=True,
     poolclass=NullPool,
-    statement_cache_size=0,
-    # connect_args={"statement_cache_size": 0},
+    connect_args={"statement_cache_size": 0},
 )
 
 
